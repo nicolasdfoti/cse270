77b7c48f7d748866cdd718936e4412238df678f0
Nicolás Foti <nicolasdfoti@gmail.com>
2025-08-07 10:56:17 -0300

    added Selenium IDE tests

--- tests/test_tetonIdahoPage.py
+++ tests/test_tetonIdahoPage.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestSmokeTest():
+class TestTetonIdahoPage():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
     self.vars = {}
@@ -18,27 +18,20 @@ class TestSmokeTest():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_tetonPageTests(self):
+  def test_directoryPage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
-    self.driver.set_window_size(1936, 1048)
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
-    assert len(elements) > 0
-    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
-    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
-    assert self.driver.title == "Teton Idaho CoC"
-    self.driver.find_element(By.LINK_TEXT, "Home").click()
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1 img")
-    assert len(elements) > 0
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight2 img")
-    assert len(elements) > 0
-    elements = self.driver.find_elements(By.LINK_TEXT, "Join Us!")
-    assert len(elements) > 0
+    self.driver.set_window_size(1296, 768)
     self.driver.find_element(By.LINK_TEXT, "Directory").click()
     self.driver.find_element(By.ID, "directory-grid").click()
-    self.driver.find_element(By.ID, "directory-selector").click()
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > img")
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(4)")
     assert len(elements) > 0
-    self.driver.find_element(By.LINK_TEXT, "Home").click()
+    self.driver.find_element(By.ID, "directory-list").click()
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)")
+    assert len(elements) > 0
+  
+  def test_joinPage(self):
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
+    self.driver.set_window_size(1296, 768)
     self.driver.find_element(By.LINK_TEXT, "Join").click()
     elements = self.driver.find_elements(By.NAME, "fname")
     assert len(elements) > 0
@@ -53,18 +46,35 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "submit").click()
     elements = self.driver.find_elements(By.NAME, "email")
     assert len(elements) > 0
+  
+  def test_adminPage(self):
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
+    self.driver.set_window_size(1296, 768)
     self.driver.find_element(By.LINK_TEXT, "Admin").click()
     elements = self.driver.find_elements(By.ID, "username")
     assert len(elements) > 0
     self.driver.find_element(By.ID, "username").click()
     self.driver.find_element(By.ID, "username").send_keys("admin")
-    self.driver.find_element(By.ID, "password").click()
     self.driver.find_element(By.ID, "password").send_keys("admin")
-    self.driver.find_element(By.LINK_TEXT, "Admin").click()
-    self.driver.find_element(By.ID, "username").click()
-    self.driver.find_element(By.ID, "username").send_keys("admin")
-    self.driver.find_element(By.ID, "password").send_keys("admin")
-    self.driver.find_element(By.ID, "password").send_keys(Keys.ENTER)
-    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
+    time.sleep(5)
+    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
+  
+  def test_homePage(self):
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
+    self.driver.set_window_size(1936, 1048)
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
+    assert len(elements) > 0
+    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
+    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
+    assert self.driver.title == "Teton Idaho CoC"
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1 > .centered-image")
+    assert len(elements) > 0
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight2")
+    assert len(elements) > 0
+    elements = self.driver.find_elements(By.LINK_TEXT, "Join Us!")
+    assert len(elements) > 0
+    self.driver.find_element(By.LINK_TEXT, "Join Us!").click()
+    elements = self.driver.find_elements(By.LINK_TEXT, "Join")
+    assert len(elements) > 0
   
